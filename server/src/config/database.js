7865504f80c91b578f4fd6f9b9e49aac40ea45f0
DavidPratama21 <davidprt202@gmail.com>
2025-09-30 15:58:39 +0700

    sama aja

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,4 +1,3 @@
-import mysql from "mysql2";
 import pkg from "pg";
 import dotenv from "dotenv";
 
@@ -16,15 +15,3 @@ export const pool = new Pool({
   connectionTimeoutMillis: 2000,
 });
 
-// export const pool = mysql
-//     .createPool({
-//         host: process.env.DB_HOST,
-//         port: process.env.DB_PORT,
-//         user: process.env.DB_USER,
-//         password: process.env.DB_PASSWORD,
-//         database: process.env.DB_NAME,
-//         waitForConnections: true,
-//         connectionLimit: 10,
-//         queueLimit: 0,
-//     })
-//     .promise();
