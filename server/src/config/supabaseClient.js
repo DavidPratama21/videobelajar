f7c34feeff217d855c1c4a0c2fd684afbea44f7d
DavidPratama21 <davidprt202@gmail.com>
2025-10-01 14:58:36 +0700

    add doteng in supabaseC.js

--- server/src/config/supabaseClient.js
+++ server/src/config/supabaseClient.js
@@ -1,7 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
+import dotenv from "dotenv";
 
+dotenv.config();
 const supabaseUrl = process.env.APP_SUPABASE_URL;
 const supabaseKey = process.env.APP_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-export default supabase
+export default supabase;
